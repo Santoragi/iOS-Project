66a34be886904cc6a3105c728193a5658e3492d6
장우진 <woojin@jang-ujin-ui-MacBookAir-2.local>
2025-06-09 17:08:16 +0900

    add Project

---
+++ MeloNote/MeloNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeloNote
+//
+//  Created by 장우진 on 6/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeloNote/MeloNote/MeloNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeloNoteApp.swift
+//  MeloNote
+//
+//  Created by 장우진 on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MeloNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
